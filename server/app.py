588d2027895f3d487c9a5ad5b2a3d97b2d283408
cjd0 <chrisdoyle20@gmail.com>
2025-03-13 22:59:35 +0000

    Added a POST request, and replaced hello world with a template for the POST function

--- server/app.py
+++ server/app.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder ='templates')
 
-@app.route("/")
-def greetings_globe():
-    return "<p>Greetings, Globe!</p>"
+@app.route('/')
+def main_page():
+    return render_template('user.html')
 
-@app.route("/chris")
+# GET
+@app.route('/chris')
 def chris():
-    return "This is a page for Chris"
+    return 'This is a page for Chris'
 
-@app.route("/matt")
+@app.route('/matt')
 def matt():
-    return "This is a page for Matt"
+    return 'This is a page for Matt'
+
+# POST
+@app.route('/user_login', methods=['POST', 'GET'])
+def user_login():
+    method = request.method
+    if method == 'POST':
+        username = request.form['username']
+        return f"Welcome {username}, you got here via a {method} request."
+    else:
+        return f"You got here via a {method} request, unnamed one."
