acbc3145be8a3dc14f09eb4546efcdd339bd771d
cjd0 <chrisdoyle20@gmail.com>
2025-04-12 22:32:54 +0100

    started constants file in game package, moved NEW_DECK into it

---
+++ server/game/constants.py
@@ -0,0 +1,70 @@
+"""
+Constant values for use in the game package
+"""
+
+# NEW_DECK represents a standard 52-card deck the format is <value><suit>:
+# SUITS:
+# C = Clubs
+# D = Diamonds
+# H = Hearts
+# S = Spades
+# VALUES:
+# X = Ace (X represents Ace for the purposes of sorting in Player.hand_value())
+# J = Jack
+# Q = Queen
+# K = King
+
+NEW_DECK = [
+    "XC",
+    "XD",
+    "XH",
+    "XS",
+    "2C",
+    "2D",
+    "2H",
+    "2S",
+    "3C",
+    "3D",
+    "3H",
+    "3S",
+    "4C",
+    "4D",
+    "4H",
+    "4S",
+    "5C",
+    "5D",
+    "5H",
+    "5S",
+    "6C",
+    "6D",
+    "6H",
+    "6S",
+    "7C",
+    "7D",
+    "7H",
+    "7S",
+    "8C",
+    "8D",
+    "8H",
+    "8S",
+    "9C",
+    "9D",
+    "9H",
+    "9S",
+    "10C",
+    "10D",
+    "10H",
+    "10S",
+    "JC",
+    "JD",
+    "JH",
+    "JS",
+    "QC",
+    "QD",
+    "QH",
+    "QS",
+    "KC",
+    "KD",
+    "KH",
+    "KS",
+]
--- server/game/game.py
+++ server/game/game.py
@@ -4,75 +4,7 @@ Module for the Game class
 
 import random
 from player import Player
-
-
-# new_deck represents a standard 52-card deck the format is <value><suit>:
-# SUITS:
-# C = Clubs
-# D = Diamonds
-# H = Hearts
-# S = Spades
-# VALUES:
-# X = Ace (X represents Ace for the purposes of sorting in Player.hand_value())
-# J = Jack
-# Q = Queen
-# K = King
-
-NEW_DECK = [
-    "XC",
-    "XD",
-    "XH",
-    "XS",
-    "2C",
-    "2D",
-    "2H",
-    "2S",
-    "3C",
-    "3D",
-    "3H",
-    "3S",
-    "4C",
-    "4D",
-    "4H",
-    "4S",
-    "5C",
-    "5D",
-    "5H",
-    "5S",
-    "6C",
-    "6D",
-    "6H",
-    "6S",
-    "7C",
-    "7D",
-    "7H",
-    "7S",
-    "8C",
-    "8D",
-    "8H",
-    "8S",
-    "9C",
-    "9D",
-    "9H",
-    "9S",
-    "10C",
-    "10D",
-    "10H",
-    "10S",
-    "JC",
-    "JD",
-    "JH",
-    "JS",
-    "QC",
-    "QD",
-    "QH",
-    "QS",
-    "KC",
-    "KD",
-    "KH",
-    "KS",
-]
-
+from constants import NEW_DECK
 
 class Game:
     """
