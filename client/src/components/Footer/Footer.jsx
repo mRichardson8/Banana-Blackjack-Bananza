dd386436add7192fef81567c525a51b2ac521e9d
Matthew Richardson <matt.rich2045@gmail.com>
2026-06-14 16:41:53 +0100

    Updated footer to dynamically set the year

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { useMemo } from "react";
 import "./Footer.css";
 const Footer = () => {
+  const year = useMemo(() => {
+    const currentYear = new Date().getUTCFullYear();
+    return currentYear;
+  }, []);
   return (
     <footer>
-      <p>© 2025 Banana Blackjack Bananza Boys inc. All Rights Reserved</p>
-      <p className="hidden">This is a licensed Mad Dog Richardson Product</p>
+      <p>© {year} Banana Blackjack Bananza Boys inc. All Rights Reserved</p>
+      <p className="hidden">This is a licensed Mad Dog Richardson & CJ Doyle Product</p>
     </footer>
   );
 };
