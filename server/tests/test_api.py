03cbee07f63506224b2192b4ed1dc0c7ee7c8f96
Chris Doyle <chrisdoyle20@gmail.com>
2026-07-08 22:04:45 +0100

    Fixed bug with newCard check not handling special bust string, and new stick test.

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -46,7 +46,27 @@ def test_player_action_twist(client):
     # correct number of cards in returned hand
     assert len(response.json["playerHand"]["cards"]) == 3
     # new card is last card in hand
-    assert response.json["playerHand"]["cards"][-1] == response.json["newCard"]
+    # checking if in newCard to handle special string that is returned for bust
+    assert response.json["playerHand"]["cards"][-1] in response.json["newCard"]
     # the value of the hand is greater than 0
     assert response.json["playerHand"]["value"] > 0
-    assert response.json["gameState"]
+    assert response.json["gameState"] in ["player_twist", "player_bust"]
+
+def test_player_action_stick(client):
+    """Test the player action route with the twist action."""
+    client.post(
+        '/start',
+        json={"playerName": "Player McPlayface"},
+        headers={'Content-type':'application/json'},
+    )
+    response = client.post(
+        '/player-action',
+        json={"playerAction": "stick"},
+        headers={'Content-type':'application/json'},
+    )
+    assert response.status_code == 200
+    # correct number of cards in returned hand
+    assert len(response.json["dealerHand"]["cards"]) >= 2
+    # the value of the hand is greater than 0
+    assert response.json["dealerHand"]["value"] > 0
+    assert response.json["gameState"] in ["player_win", "dealer_win", "draw"]
