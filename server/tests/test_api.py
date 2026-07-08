a393d042f0a0c3bc0ae2db599a040aee8f8fdd63
Chris Doyle <chrisdoyle20@gmail.com>
2026-07-08 21:52:34 +0100

    Fixed card length test bug in test_start and added player action twist test.

--- server/tests/test_api.py
+++ server/tests/test_api.py
@@ -22,8 +22,31 @@ def test_start(client):
     assert len(response.json["gameId"]) == 6
     # the hand has two cards
     assert len(response.json["playerHand"]["cards"]) == 2
-    # the cards are made up of two characters
-    assert len(response.json["playerHand"]["cards"][0]) == 2
-    assert len(response.json["playerHand"]["cards"][1]) == 2
+    # the cards are not empty strings
+    assert response.json["playerHand"]["cards"][0] 
+    assert response.json["playerHand"]["cards"][1]
     # the value of the hand is greater than 0
     assert response.json["playerHand"]["value"] > 0
+
+def test_player_action_twist(client):
+    """Test the player action route with the twist action."""
+    client.post(
+        '/start',
+        json={"playerName": "Dr Leo Spaceman"},
+        headers={'Content-type':'application/json'},
+    )
+    response = client.post(
+        '/player-action',
+        json={"playerAction": "twist"},
+        headers={'Content-type':'application/json'},
+    )
+    assert response.status_code == 200
+    # new card is not missing
+    assert response.json["newCard"]
+    # correct number of cards in returned hand
+    assert len(response.json["playerHand"]["cards"]) == 3
+    # new card is last card in hand
+    assert response.json["playerHand"]["cards"][-1] == response.json["newCard"]
+    # the value of the hand is greater than 0
+    assert response.json["playerHand"]["value"] > 0
+    assert response.json["gameState"]
